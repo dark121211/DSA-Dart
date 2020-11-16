109b955df5ffe38ec4082de558a610d18b379abe
vipuluthaiah <kechamadavipul@gmail.com>
2020-11-16 10:25:34 +0530

    test pull

--- 5probelemsolving/01.dart
+++ 5probelemsolving/01.dart
@@ -13,28 +13,28 @@ main() {
 }
 
 same(List array1, List array2) {
-  // if (array1.length != array2.length) {
-  //   return false;
-  // }
+  if (array1.length != array2.length) {
+    return false;
+  }
 
-  // var frequencyCounter1 = {};
-  // var frequencyCounter2 = {};
-  // for (var val in array1) {
-  //   frequencyCounter1[val] =( (frequencyCounter1[val] || 0) == 1);
-  // }
+  var frequencyCounter1 = {};
+  var frequencyCounter2 = {};
+  for (var val in array1) {
+    frequencyCounter1[val] =( (frequencyCounter1[val] || 0) == 1);
+  }
 
-  // if (!(array1.length == array2.length)) {
-  //   return "Array dont match";
-  // } else {
-  //   for (var i = 0; i < array1.length; i++) {
-  //     var correctIndex = (array1.indexOf(array1[i] * 2));
-  //     if (correctIndex == -1) {
-  //       return "negative";
-  //     }
-  //     print("$array2");
-  //     // array2.splice(correctIndex,1);
-  //     array2.sublist(correctIndex, 1);
-  //   }
-  //   return true;
-  // }
+  if (!(array1.length == array2.length)) {
+    return "Array dont match";
+  } else {
+    for (var i = 0; i < array1.length; i++) {
+      var correctIndex = (array1.indexOf(array1[i] * 2));
+      if (correctIndex == -1) {
+        return "negative";
+      }
+      print("$array2");
+      // array2.splice(correctIndex,1);
+      array2.sublist(correctIndex, 1);
+    }
+    return true;
+  }
 }
